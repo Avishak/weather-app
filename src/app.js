a2e577de6975fbd634cd9124d5ce372f9c69a71e
Avishak <avishaktripathi@gmail.com>
2020-01-08 16:33:55 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -22,14 +22,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather Report',
-        name: 'Andrew Mead'
+        name: 'Avishak Tripathi'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'About Me',
-        name: 'Andrew Mead'
+        title: 'About Us',
+        name: 'Avishak Tripathi'
     })
 })
 
@@ -37,7 +37,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Avishak Tripathi'
     })
 })
 
@@ -83,7 +83,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Avishak Tripathi',
         errorMessage: 'Help article not found.'
     })
 })
@@ -91,7 +91,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Avishak Tripathi',
         errorMessage: 'Page not found.'
     })
 })
